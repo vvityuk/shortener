ddf5821cbd012c18cbc5f68feebfef502efaab45
Victor Vityuk <vicv@1-vc.ru>
2024-12-21 02:34:13 +0300

    Первичные наброски методов

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,72 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+var mylist = make(map[string]string)
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+
+	// Получение ссылки по сокращенному коду
+	if r.Method == http.MethodGet {
+
+		val, ok := mylist[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, val, http.StatusMovedPermanently)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		return
+	}
+
+	// Получение сокращенной ссылки
+	if r.Method == http.MethodPost {
+		defer r.Body.Close()
+		myurl := make([]byte, 1024)
+		n, _ := r.Body.Read(myurl)
+		ok := true
+		shortUrl := ""
+		for ok {
+			shortUrl = randStr(4)
+			_, ok = mylist["/"+shortUrl]
+		}
+		mylist["/"+shortUrl] = string(myurl[:n])
+
+		//fmt.Print(string(myurl))
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(shortUrl))
+		return
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	return
+
+}
+
+func main() {
+
+	http.HandleFunc(`/`, mainPage)
+	err := http.ListenAndServe(`:8080`, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func randStr(n int) string {
+
+	rnd := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+
+	letters := []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rnd.Intn(len(letters))]
+	}
+	return string(b)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vvityuk/shortener
 
 go 1.23.4
+
+require golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
