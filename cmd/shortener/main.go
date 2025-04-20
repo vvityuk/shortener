b65199d3dbd14bfdf07d17644277791407a22a8d
Victor Vityuk <vicv@1-vc.ru>
2025-04-21 02:04:25 +0300

    добавил логирование

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/vvityuk/shortener/internal/app"
+	"github.com/vvityuk/shortener/internal/app/middleware"
 	"github.com/vvityuk/shortener/internal/config"
+	"go.uber.org/zap"
 )
 
 func main() {
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync()
+
 	cfg, err := config.NewConfig()
 	if err != nil {
 		panic("Failed to initialize config")
@@ -18,7 +28,7 @@ func main() {
 	handler := app.NewHandler(service)
 
 	r := chi.NewRouter()
-
+	r.Use(middleware.LoggingMiddleware(logger))
 	// Роуты
 	r.Get("/{shortCode}", handler.GetURL)
 	r.Post("/", handler.CreateURL)
